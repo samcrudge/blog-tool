3aa0665d119da6b2e9ea154eeca201a51ef0ef1b
SamCrudge <samuelcrudge@gmail.com>
2021-03-11 14:53:30 +0000

    Fixed query so its more verbose

--- src/Models/BlogModel.php
+++ src/Models/BlogModel.php
@@ -15,32 +15,29 @@ class BlogModel
         $this->db = $db;
     }
 
-    public function GetAllEntries()
+    public function GetAllEntries(): array
     {
-        $query = $this->db->query("SELECT `*` FROM `blog-posts` WHERE `deleted` = 0;");
+        $query = $this->db->query("SELECT `title`, `author`, `date`, `post`, `GUID`
+                                    FROM `blog-posts` 
+                                    WHERE `deleted` = 0;");
          return $query = $query->fetchAll();
 
     }
 
-    /**
-     * @return bool
-     * Creates a new post with Prepare to avoid Mysql injection.
-     */
+
     public function CreateNewEntry($BlogPost): bool
     {
-        $query = $this->db->prepare("INSERT INTO `blog-posts` (`title`, `author`, `date`, `post`) VALUE (:title, :author, :date, :post, :GUID)");
+        $query = $this->db->prepare("INSERT INTO `blog-posts` (`title`, `author`, `date`, `post`) 
+                                        VALUE (:title, :author, :date, :post, :GUID)");
         $addNewEntry = $query->execute($BlogPost);
         return $addNewEntry;
     }
 
-    /**
-     * @return bool
-     * Collects selected Post via `GUID` and allows update to `Title` & `Post`.
-     * Uses Prepare to avoid Mysql Injection.
-     */
+
     public function EditEntry($EditEntry): bool
     {
-        $query = $this->db->prepare("SELECT `GUID` FROM `blog-posts` UPDATE (:title, :Date, :post)");
+        $query = $this->db->prepare("SELECT `GUID` FROM `blog-posts` 
+                                        UPDATE (:title, :Date, :post)");
         $updatedEntry = $query->execute($EditEntry);
         return $updatedEntry;
     }
@@ -50,7 +47,7 @@ class BlogModel
      * Collects selected Post via `GUID` and soft deletes.
      * Uses Prepare to avoid Mysql Injection.
      */
-    public function DeleteEntry($deleteEntry)
+    public function DeleteEntry($deleteEntry): bool
     {
         $query = $this->db->prepare("SELECT `GUID` FROM `blog-posts` UPDATE (:deleted)");
         $deletedEntry = $query->execute($deleteEntry);
